c6ccde177920982aa56d7cdbeb89b8be95f5364d
declanm.mulligan@gmail.com Mulligan <declanm.mulligan@gmail.com>
2017-12-22 19:52:02 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -7,7 +7,7 @@ def get_first_name_of_season_winner(data, season)
 end
 
 def get_contestant_name(data, occupation)
-  data[season].each do |season,contestants|
+  data.each do |season,contestants|
     contestant.each do |contestant_stats|
       if contestant_stats["occupation"]==occupation
         return contestant_stats["name"]
