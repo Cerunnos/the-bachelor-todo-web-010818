961e12d7583e33e84bc5ce8e380d673ec5a70724
declanm.mulligan@gmail.com Mulligan <declanm.mulligan@gmail.com>
2017-12-22 20:15:45 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -39,7 +39,7 @@ def get_occupation(data, hometown)
 end
 
 def get_average_age_for_season(data, season)
-  age=0
+  sum_of_age=0
   contenstant_num=0
   data[season].each do |contestants|
     age+=contestants["age"].to_i
